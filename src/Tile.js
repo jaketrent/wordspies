61bb6e1ed3d9a5602b1c12bfd5428f9eb3071a0c
jaketrent <trent.jake@gmail.com>
2016-12-31 16:02:51 -0700

    make tile color more obvious

--- src/Tile.js
+++ src/Tile.js
@@ -4,10 +4,19 @@ import css from './Tile.css'
 const { bool, func, shape, string } = React.PropTypes
 
 class Tile extends React.Component {
+  getClassName() {
+    const colors = {
+      r: css.colorRed,
+      b: css.colorBlue,
+      n: css.colorNeutral,
+      a: css.colorAssassin,
+    }
+    return this.props.tile.faceup ? colors[this.props.tile.color] : css.root
+  }
   render() {
     return (
-      <div onClick={this.props.onClick} className={css.root}>
-        {this.props.tile.word} {this.props.tile.faceup ? '(' + this.props.tile.color + ')' : ''}
+      <div onClick={this.props.onClick} className={this.getClassName()}>
+        {this.props.tile.word}
       </div>
     )
   }
